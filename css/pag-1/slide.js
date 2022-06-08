f3fdd8eab02709c71ca0197bf58a167be46a6f84
Marcos Coelho <mparav124@gmail.com>
2022-06-08 00:43:33 -0300

    style e js slide

---
+++ css/pag-1/slide.js
@@ -0,0 +1,23 @@
+let timer = 3000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll('.slide-container img'),
+    max = images.length;
+
+function slider() {
+  images[currentImageIndex].classList.remove('active')
+
+  currentImageIndex++
+  if (currentImageIndex>=max) {
+    currentImageIndex = 0;
+  }
+
+  images[currentImageIndex].classList.add('active');
+}
+
+function start() {
+  setInterval(() => {
+    slider()
+  }, timer)
+}
+
+window.addEventListener('load', start)
