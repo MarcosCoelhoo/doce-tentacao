e25ad7324e19d47522c256d74bcd90d1eb082e7a
Marcos Coelho <mparav124@gmail.com>
2022-07-05 13:28:30 -0300

    atualizações

---
+++ css/system.js
@@ -0,0 +1,14 @@
+function Carregando()
+{
+	smscarregando.style.display='none';
+	pagina.style.display='block';
+}
+
+function entrar()
+{
+	document.writeln('<img src="imgss/loading.gif">A carregar...');
+	//newWindow=window.open("http://www.docetencao.com","_top");
+}
+
+/*--------/\ defenir as funcoes ---- \/ executalas ----- -*/
+Carregando();
